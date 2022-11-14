d0b4a0f7f772ea18fcfedeee0382ffcb6912f63e
poposhka1990 <popovemail1990@gmail.com>
2022-11-14 20:58:14 +0300

    add new components from React lesson 2

---
+++ src/components/MyComponent/MyComponent.js
@@ -0,0 +1,13 @@
+import './MyComponent.css';
+
+function MyConponent() {
+    return (
+        <div>
+            <span>Hello,</span>
+            <span> functional</span>
+            <span> component!</span>
+        </div>
+    )
+}
+
+export default MyConponent;
