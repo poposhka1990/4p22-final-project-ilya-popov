fa822928eb305ed14565cde453b03e291996ccef
poposhka1990 <popovemail1990@gmail.com>
2022-12-05 20:25:56 +0300

    correct

--- src/config.js
+++ src/config.js
@@ -1,17 +1,7 @@
 const BASE_URL = 'https:/restcountries.com/v2/';
 
-<<<<<<< HEAD
-export const ALL_COUNTRIES = BASE_URL + 'all';
-
-export const searchByCountry = (name) => BASE_URL + 'name/' + name;
-
-export const filterByCode = (codes) => BASE_URL + 'alpha?codes=' + codes.join(',');
-=======
 export const ALL_COUNTRIES = BASE_URL + 'all?fields=name,capital,flags,population,region';
 
 export const searchByCountry = (name) => BASE_URL + 'name/' + name;
 
 export const filterByCode = (codes) => BASE_URL + 'alpha?codes=' + codes.join(',');
-
-export const fullLink = 'https://restcountries.com/v2/all?fields=name,capital,flags,population,region';
->>>>>>> 8dd84c2e092a9847d708ed1007a82ccb580f8488
--- src/index.js
+++ src/index.js
@@ -9,16 +9,9 @@ import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
-    <BrowserRouter basename='4p22-final-project-ilya-popov'>
-        <App />
-    </BrowserRouter>
-);
-=======
     <Provider store={store}>
-        <BrowserRouter basename='4p22-final-project-ilya-popov'>
+        <BrowserRouter basename='4p22-final-project-countries-ilya-popov'>
             <App />
         </BrowserRouter>
     </Provider>
 );
->>>>>>> 8dd84c2e092a9847d708ed1007a82ccb580f8488
